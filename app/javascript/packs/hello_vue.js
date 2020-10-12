7c5b8b7db1479549e0afc9bfe30b2969337a0b8f
tetsu <banginhoodyonezawa@gmail.com>
2020-10-12 22:56:36 +0900

    Hello Vue! 表示 to root to: 'home#index'

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -11,10 +11,7 @@ import App from '../app.vue'
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
-
-  console.log(app)
+  }).$mount("#app")
 })
 
 
